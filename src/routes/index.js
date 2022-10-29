49424c32bed1de9439fd8767c77230c5f0e4addc
chaechae <dokailove6326@gmail.com>
2022-10-29 21:40:47 +0900

    Fix: NotFound 추가

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import List from "@/pages/List";
 import Detail from "@/pages/Detail";
+import NotFound from "@/pages/NotFound";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const Router = () => {
@@ -9,6 +10,7 @@ const Router = () => {
       <Routes>
         <Route index path="/" element={<List />} />
         <Route path="/:id" element={<Detail />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
