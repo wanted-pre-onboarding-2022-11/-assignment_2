421374983fd0aeb663d85ea485b834b791190b1d
yong <tkddbs1644@gmail.com>
2022-10-30 17:55:29 +0900

    Feat: Scroll 상태를 유지하는 기능 구현

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { useRef } from "react";
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import { IssueContext } from "../contexts/IssueContext";
 
 const Home = () => {
+  const navigate = useNavigate();
+  const [scrollHeight, setScrollHeight] = useState(0);
+  const containerRef = useRef(null);
   const { issues, fetchNextPage } = useContext(IssueContext);
 
+  const handleClickIssue = (id) => {
+    navigate(`detail/${id}`);
+    setScrollHeight(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.scrollTo({
+      top: scrollHeight,
+      behavior: "smooth",
+    });
+    //eslint-disable-next-line
+  }, []);
+
   return (
-    <>
+    <div ref={containerRef}>
       {issues.map((issue) => (
-        <li key={issue.id}>{issue.title}</li>
+        <li onClick={handleClickIssue.bind(this, issue.id)} key={issue.id}>
+          {issue.title}
+        </li>
       ))}
       <button onClick={fetchNextPage}>다음 페이지(무한스크롤)</button>
-    </>
+    </div>
   );
 };
 
