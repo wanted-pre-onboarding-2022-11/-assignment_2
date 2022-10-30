ba9cb476b62be0d06dca711b870f41de05bf56b7
0SCAR0421 <oscar0421@kakao.com>
2022-10-30 16:11:19 +0900

    Feat: IssueDetail 컴포넌트 기능구현 및 스타일링

---
+++ src/pages/IssueDetail/IssueDetail.styled.js
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+import { IssuesContainer } from "../Issues/Issues.styled";
+
+export const IssueDetailContaier = styled(IssuesContainer)`
+  > div {
+    width: 100%;
+  }
+
+  > div:first-child > div {
+    padding-bottom: 30px;
+  }
+
+  > div:last-child > code {
+    position: relative;
+  }
+`;
+
+export const IssueInfomation = styled.div`
+  display: grid;
+  gap: 30px;
+  grid-template-columns: 50px auto 70px;
+  border-bottom: 1px solid #ccc;
+
+  > div {
+    display: flex;
+  }
+
+  > div:first-child {
+    align-self: center;
+  }
+
+  > div:nth-child(2) {
+    display: block;
+    word-break: break-all;
+  }
+
+  > div:nth-child(2) > div:first-child {
+    padding-bottom: 15px;
+  }
+
+  > div:last-child {
+    align-self: center;
+  }
+`;
+
+export const UserAvatar = styled.div`
+  background-image: url(${(props) => props.avatar});
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 50px;
+  width: 50px;
+`;
--- src/pages/IssueDetail/index.js
+++ src/pages/IssueDetail/index.js
@@ -1,18 +1,55 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { getIssue } from "@apis";
+import {
+  IssueDetailContaier,
+  IssueInfomation,
+  UserAvatar,
+} from "@pages/IssueDetail/IssueDetail.styled";
+import { Loading } from "../Issues/Issues.styled";
+import { convertDay } from "@utils";
+import MarkdownPreview from "@uiw/react-markdown-preview";
 
 const IssueDetail = () => {
   const navigate = useNavigate();
   const { issuenumber } = useParams();
+  const [issue, setIssue] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    getIssue(issuenumber).then((res) => {
-      if (res.state !== "open") navigate("/error");
-    });
+    setLoading(true);
+    getIssue(issuenumber)
+      .then((res) => {
+        if (res.state !== "open") navigate("/error");
+        setIssue(res);
+        setLoading(false);
+      })
+      .catch((e) => {
+        navigate("/error");
+      });
   }, [issuenumber, navigate]);
 
-  return <div></div>;
+  if (loading) return <Loading>로딩중...</Loading>;
+
+  return (
+    <IssueDetailContaier>
+      <div>
+        <IssueInfomation>
+          <UserAvatar avatar={issue.user.avatar_url} />
+          <div>
+            <div>
+              #{issue.number} {issue.title}
+            </div>
+            <div>
+              작성자: {issue.user.login}, 작성일: {convertDay(issue.created_at)}
+            </div>
+          </div>
+          <div>코멘트: {issue.comments}</div>
+        </IssueInfomation>
+      </div>
+      <MarkdownPreview source={issue.body} />
+    </IssueDetailContaier>
+  );
 };
 
 export default IssueDetail;
