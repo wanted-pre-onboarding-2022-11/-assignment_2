2fcf158151250af45bbf92514b6eda3851ecd21a
0SCAR0421 <oscar0421@kakao.com>
2022-10-30 16:14:51 +0900

    Feat: Issues 컴포넌트 기능구현 및 스타일링

--- src/pages/Issues/Issues.styled.js
+++ src/pages/Issues/Issues.styled.js
@@ -1,7 +1,28 @@
 import styled from "styled-components";
 
 export const IssuesContainer = styled.div`
-  background: royalblue;
-  height: 100vh;
-  width: 100%;
+  font-size: 14px;
+  color: #4e4e4e;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  margin: 50px 0;
+
+  > div {
+    padding: 30px;
+    max-width: 700px;
+  }
+`;
+
+export const Loading = styled.div`
+  display: flex;
+  justify-content: center;
+  font-size: 24px;
+`;
+
+export const Notification = styled.div`
+  padding-top: 50px;
+  display: flex;
+  justify-content: center;
+  font-size: 16px;
 `;
--- src/pages/Issues/index.js
+++ src/pages/Issues/index.js
@@ -1,8 +1,8 @@
 import React, { useContext, useRef } from "react";
 import { IssueListContext } from "@utils";
-import { IssuesContainer } from "@pages/Issues/Issues.styled";
+import { IssuesContainer, Notification, Loading } from "@pages/Issues/Issues.styled";
 import useInfinityScroll from "@hooks/useInfinityScroll";
-import { Issue } from "@components";
+import { Issue, Error } from "@components";
 
 const Issues = () => {
   const { issueList, isLoading, isEndData, isError, handleNextPage } = useContext(IssueListContext);
@@ -11,23 +11,25 @@ const Issues = () => {
 
   useInfinityScroll(obsRef, isLoading, isEndData, handleNextPage);
 
-  if (isError) return <div>에러가 발생했어요!</div>;
+  if (isError) return <Error />;
 
   return (
     <IssuesContainer>
-      {issueList.map((e, i) => (
-        <Issue
-          key={e.id}
-          issuenumber={e.number}
-          title={e.title}
-          owner={e.user.login}
-          createdAt={e.created_at}
-          comments={e.comments}
-          isAdvertisement={i === 3}
-        ></Issue>
-      ))}
-      {isEndData && <div>더 이상 불러올 데이터가 없습니다.</div>}
-      <div ref={obsRef}>{isLoading && <div>로딩중...</div>}</div>
+      <div>
+        {issueList.map((e, i) => (
+          <Issue
+            key={e.id}
+            issuenumber={e.number}
+            title={e.title}
+            owner={e.user.login}
+            createdAt={e.created_at}
+            comments={e.comments}
+            isAdvertisement={i === 3}
+          ></Issue>
+        ))}
+        {isEndData && <Notification>더 이상 불러올 데이터가 없습니다.</Notification>}
+        <div ref={obsRef}>{isLoading && <Loading>로딩중...</Loading>}</div>
+      </div>
     </IssuesContainer>
   );
 };
