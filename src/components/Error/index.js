6b3973ae8514b782a1db1d1c8e848e21e57af6cd
0SCAR0421 <oscar0421@kakao.com>
2022-10-30 16:08:00 +0900

    Feat: Error 컴포넌트 작성

--- src/components/Error/index.js
+++ src/components/Error/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { ErrorContainer } from "@components/Error/Error.styled";
 
 const Error = () => {
   const navigate = useNavigate();
 
   return (
-    <div>
+    <ErrorContainer>
       <h1>404 NOT FOUND</h1>
       <button onClick={() => navigate("/")}>메인으로</button>
-    </div>
+    </ErrorContainer>
   );
 };
 
