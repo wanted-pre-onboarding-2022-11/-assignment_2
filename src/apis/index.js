73d0389a4d05f61029b6603aa6b2cb160971443b
yong <tkddbs1644@gmail.com>
2022-10-30 16:33:20 +0900

    Feat: issue를 불러오는 api 추가

---
+++ src/apis/index.js
@@ -0,0 +1,22 @@
+import { Octokit } from "octokit";
+
+const { REACT_APP_AUTH_KEY } = process.env;
+
+const octokit = new Octokit({
+  auth: REACT_APP_AUTH_KEY,
+});
+
+const getIssuesByPage = async (page = 1) => {
+  const data = await octokit.request("GET /repos/angular/angular-cli/issues", {
+    owner: "OWNER",
+    repo: "REPO",
+    sort: "comments",
+    page,
+  });
+  if (data.status === 200) {
+    return data.data;
+  }
+  throw new Error("fetch error.");
+};
+
+export default getIssuesByPage;
