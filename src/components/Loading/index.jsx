d058126a898a249a4ae3954054e1c5566e9733a2
0SCAR0421 <oscar0421@kakao.com>
2022-10-30 22:14:10 +0900

    Feat: 로딩 컴포넌트 구현 및 매핑

---
+++ src/components/Loading/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { LoadingSpinner } from "@components/Loading/LoadingSpinner.styled";
+
+const Loading = () => {
+  return <LoadingSpinner />;
+};
+
+export default Loading;
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as IssueItem } from "@components/IssueItem";
 export { default as Error } from "@components/Error";
+export { default as Loading } from "@components/Loading";
